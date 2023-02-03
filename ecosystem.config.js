b211a20c36727297ebfed4578bf6807c0fd956ce
chp9419 <chp.code@gmail.com>
2023-02-03 17:27:33 +0900

    :Add:pm2 설정

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "pro",
+      script: "./app.js",
+      watch: false,
+      // node_args: '--resolveJsonModule'
+      // instances: 4,
+      // exec_mode:'cluster'
+    },
+  ],
+};
+
